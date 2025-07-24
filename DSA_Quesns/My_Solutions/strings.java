da9d48e61e8f4b8058175680ee37be873a3aab6a
Sumit Desai <sumit.desai3433@gmail.com>
2025-07-24 10:14:24 +0530

    Added string solutions

--- DSA_Quesns/My_Solutions/strings.java
+++ DSA_Quesns/My_Solutions/strings.java
@@ -86,3 +86,46 @@ public class strings {
 }
  */
 }
+
+// Longest Common Prefix
+/*
+public class strings {
+    public static void main(String[] args) {
+        String[] strs = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+    static String longestCommonPrefix(String[] strs) {
+        StringBuilder sb = new StringBuilder();
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+
+        for(int i = 0; i < Math.min(first.length(), last.length()); i++){
+            if(first.charAt(i) != last.charAt(i))
+                return sb.toString();
+            sb.append(first.charAt(i));
+        }
+        return sb.toString();
+    }
+}
+*/
+
+
+// Reverse The Words in String
+/*
+public class strings {
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+        System.out.println(reverseWords(s));
+    }
+
+    static String reverseWords(String s) {
+        String[] words = s.split("[  ]+");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]).append(" ");
+        }
+        return sb.toString().trim();
+    }
+}
+ */
