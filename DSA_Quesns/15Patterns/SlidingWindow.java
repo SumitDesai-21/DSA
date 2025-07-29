29463b4e5323a4e7bf6714f9d548fca58a245084
Sumit Desai <sumit.desai3433@gmail.com>
2025-07-29 07:59:24 +0530

    Added SldingWindow Quesns

--- DSA_Quesns/15Patterns/SlidingWindow.java
+++ DSA_Quesns/15Patterns/SlidingWindow.java
@@ -12,4 +12,26 @@ public class SlidingWindow {
         }
         return (double) maxSum / k;
     }
+     // Longest Substring Without Repeating Characters
+    // left is sliding pointer
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int l = 0, Maxlen = 0;
+        HashSet<Character> set = new HashSet<>();
+        for (int i = 0; i < n; i++) {
+            if(!set.contains(s.charAt(i))){
+                set.add(s.charAt(i));
+                // i - l + 1 => len of substring
+                Maxlen = Math.max(Maxlen, i - l + 1);
+            }
+            else{
+                while(set.contains(s.charAt(i))){
+                    set.remove(s.charAt(l));
+                    l++;
+                }
+                set.add(s.charAt(i));
+            }
+        }
+        return Maxlen;
+    }
 }
